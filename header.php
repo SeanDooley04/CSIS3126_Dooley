b02d467122fe5731bca32631d967a7c618b14ff0
Sean Dooley <seandooley04@gmail.com>
2025-01-30 20:14:17 -0500

    adding website code with header to Github

---
+++ header.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html>
+	<head>
+	<style>
+		
+        .rainbow-letters {
+            font-size: 40px;
+            font-family: sans-serif;
+            text-transform: uppercase;
+        }
+        .rainbow-letters span:nth-child(10n + 1) {
+            color: #ef5350;`
+        }
+        .rainbow-letters span:nth-child(10n + 2) {
+            color: #ab47bc;
+        }
+
+        .rainbow-letters span:nth-child(10n + 3){
+            color: #651fff;
+        }
+
+        .rainbow-letters span:nth-child(10n + 5){
+        color: #3949ab;
+        }
+
+        .rainbow-letters span:nth-child(10n + 4) {
+        color: #2196f3;
+        }
+
+        .rainbow-letters span:nth-child(10n + 6){
+        color: #00bcd4;
+        }
+
+        .rainbow-letters span:nth-child(10n + 7){
+        color: #4caf50;
+        }
+
+        .rainbow-letters span:nth-child(10n + 8){
+        color: #ffc107;
+        }
+
+        h1{
+			text-align: center;
+			font-family: sans-serif;
+			font-size: 3rem;
+            text-transform:uppercase;
+            
+            text-shadow: -4px 4px black;
+
+
+		}
+		body{
+			font-family: Calibri;
+			background-color: lightgray;
+			color: green;
+		}
+		table{
+			
+			color: black;
+			background-color: white;
+			
+		}
+		a{
+			color: darkolivegreen;
+		}
+
+	</style>
+		<title>Web Board Game</title>
+	</head>
+	<body>
+		
+		<h1 class="rainbow-letters">
+            <span>W</span>
+            <span>e</span>
+            <span>b</span> 
+            <span>_</span>
+            <span>B</span>
+            <span>o</span>
+            <span>a</span>
+            <span>r</span>
+            <span>d</span>
+            <span>_</span>
+            <span>G</span>
+            <span>a</span>
+            <span>m</span>
+            <span>e</span>
+
+        </h1>
